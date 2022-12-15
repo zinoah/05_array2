ed5b5f98af891be9bd2561850b093f8c7e437d7e
Jinho <zinoah@gmail.com>
2022-12-15 12:37:46 +0900

    array2 commit

---
+++ src/edu/kh/array/ex/Array2Run.java
@@ -0,0 +1,16 @@
+package edu.kh.array.ex;
+
+public class Array2Run {
+
+	public static void main(String[] args) {
+		
+		Array2Example arr2 = new Array2Example();
+		
+		//arr2.ex1();
+		//rr2.ex2();
+		arr2.ex3();
+		//arr2.ex2();
+
+	}
+
+}
