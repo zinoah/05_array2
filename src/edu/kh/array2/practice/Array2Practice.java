563ce9a645c31654dec87a8ce5a5b1c9e9c838fb
Jinho <zinoah@gmail.com>
2022-12-27 15:18:27 +0900

    third commit

--- src/edu/kh/array2/practice/Array2Practice.java
+++ src/edu/kh/array2/practice/Array2Practice.java
@@ -205,7 +205,7 @@ public class Array2Practice {
 		for(int i = 0; i < arr.length; i++) {
 			for(int j = 0; j < arr[i].length; j++) {
 				
-				System.out.println(arr[i][j] );
+				arr[i][j] = 
 			}
 		}
 	}
